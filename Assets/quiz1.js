cb164217d6938a1a5d536a6d487e509891b7c67c
Brianna Bullock <brianna.bullock16@gmail.com>
2020-05-30 00:16:18 -0500

    stopped timer at 0

--- Assets/quiz1.js
+++ Assets/quiz1.js
@@ -1,13 +1,17 @@
+// timer variables
 var timerEl = document.querySelector(".time");
+var timeDown = 5;
 
-
-var timeDown = 75;
-
+// Timer Function
 function newTimer() {
   var timerInterval = setInterval(function() {
     timeDown--;
     timerEl.innerHTML = "Time: " + timeDown;
 
+    if (timeDown <= 0) {
+        clearInterval(timerInterval);
+        return;
+    }
   }, 1000);
   
 }
